ddfd370184a433e64e29b815a20214bb9b2e65ce
Dani <danielvs90@gmail.com>
2024-10-02 12:55:54 +0100

    PGV

---
+++ src/UT2/Teoria/MiBuffer.java
@@ -0,0 +1,13 @@
+package UT2.Teoria;
+
+public class MiBuffer {
+    int contador = 0;
+
+    public void inc() {
+        contador++;
+    }
+
+    public void dec() {
+        contador--;
+    }
+}
---
+++ src/UT2/Teoria/ejemploHilos.java
@@ -0,0 +1,36 @@
+package UT2.Teoria;
+
+import java.util.Scanner;
+
+public class ejemploHilos extends Thread {
+    MiBuffer mb;
+
+    public ejemploHilos(MiBuffer mb) {
+        this.mb = mb;
+    }
+
+    // El start ejecuta el run
+    public void run() {
+        mb.inc();
+        System.out.println("Hola " + Thread.currentThread().getName());
+        System.out.println(mb.contador);
+//        Scanner sc = new Scanner(System.in);
+//        String valor = sc.nextLine();
+//        System.out.println("Soy " + Thread.currentThread().getName() + ", y mi valor es " + valor);
+    }
+
+    public static void main(String[] args) {
+        MiBuffer mb = new MiBuffer();
+
+        for (int i = 0; i < 4; i++) {
+            Thread hilo = new ejemploHilos(mb);
+            hilo.setName("Hilo " + i);
+            hilo.start();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
